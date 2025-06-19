57ed43e3fae39fd5c8cea93e8db67aee733df579
Dheeraj-kumar001 <147137829+Dheeraj-kumar001@users.noreply.github.com>
2025-06-19 11:32:49 +0000

    update Subnet.tf

---
+++ Publicip.tf
@@ -0,0 +1,8 @@
+resource "azurerm_public_ip" "main" {
+  name                = "${var.environment}-${var.public_ip_name}"
+  location            = data.azurerm_resource_group.bastion.location
+  resource_group_name = data.azurerm_resource_group.bastion.name
+  allocation_method   = var.pubip_allocation_method
+  sku                 = var.pubip_sku
+  tags                = var.default_tags
+}
